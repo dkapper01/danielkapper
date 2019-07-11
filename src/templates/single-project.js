dc93b88f0d8c7bda32c8cab4997859d7b89de488
Daniel Kapper <dkapper01@gmail.com>
2019-07-11 16:41:05 -0400

    clean up code

--- src/templates/single-project.js
+++ src/templates/single-project.js
@@ -24,47 +24,8 @@ class BlogPostTemplate extends React.Component {
           <Banner title={post.frontmatter.title} subtitle="" />
         </Header>
         <ProjectWrapper>
-          {/* <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        > */}
-          {/* {post.frontmatter.date}
-        </p> */}
+          
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          {/* <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        /> */}
-
-          {/* <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={`blog${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`blog${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul> */}
         </ProjectWrapper>
       </Layout>
     )
@@ -76,10 +37,6 @@ export default BlogPostTemplate
 const ProjectWrapper = styled.div`
   margin: 0 auto;
   width: 960px;
-  .gatsby-resp-image-image {
-    margin-top: -450px;
-    z-index: 100;
-  }
 `
 
 export const pageQuery = graphql`
